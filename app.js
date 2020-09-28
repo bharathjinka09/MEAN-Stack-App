338a5fc0d809d7a5ccb87d5898528ea0a6b6ffb9
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-28 17:01:22 +0530

    MEAN

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+mongoose.connect(config.database, (err) => {
+  if (!err) console.log("MongoDB connection succeeded.");
+  else
+    console.log(
+      "Error in DB connection : " + JSON.stringify(err, undefined, 2)
+    );
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to db" + config.database);
+});
+
+const app = express();
+
+const users = require("./routes/users");
+
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./config/passport")(passport);
+
+app.use("/users", users);
+
+app.get("/", (req, res) => {
+  res.send("invalid");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
