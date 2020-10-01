71bb6b619218bd7ddee4b4cb31ed17bd90586229
bharathjinka09 <bharathjinka09@gmail.com>
2020-10-01 11:45:37 +0530

    added changes

--- models/user.js
+++ models/user.js
@@ -36,7 +36,9 @@ module.exports.addUser = (newUser, callback) => {
 
 module.exports.comparePassword = (candidatePassword, hash, callback) => {
   bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
     callback(null, isMatch);
   });
 };
